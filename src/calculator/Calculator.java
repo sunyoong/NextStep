81fe6c7ada490c8a6a5702333737ddb4e4325c57
sunyoong <haert2010@naver.com>
2022-05-28 15:53:51 +0900

    main 클래스를 활용한 테스트

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+public class Calculator {
+	// 덧하기, 빼기, 나누기, 곱하기 메서드 만들기
+	
+	int add(int i, int j) {
+		return i+j;
+	}
+	
+	int substract(int i, int j) {
+		return i-j;
+	}
+	
+	int divide(int i, int j) {
+		return i/j;
+	}
+	
+	int multiple(int i, int j) {
+		return i*j;
+	}
+	
+	
+	
+}
