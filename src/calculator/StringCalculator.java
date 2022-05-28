c1a5535533db233aa53f97e7ccbeda9490dc1187
sunyoong <haert2010@naver.com>
2022-05-28 16:40:05 +0900

    문자열 계산기 테스트1

--- src/calculator/StringCalculator.java
+++ src/calculator/StringCalculator.java
@@ -1,5 +1,15 @@
 package calculator;
 
+import static org.junit.Assert.assertNotNull;
+
 public class StringCalculator {
 
+		public int add(String text) {
+			if(text==null || text.isEmpty()) {
+				return 0;
+			}
+			else {
+				return Integer.parseInt(text);
+			}
+		}
 }
--- test/calculator/StringCalculatorTest.java
+++ test/calculator/StringCalculatorTest.java
@@ -1,5 +1,27 @@
 package calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class StringCalculatorTest {
 
+	private StringCalculator cal;
+	
+	@BeforeEach
+	public void setup() {
+		cal = new StringCalculator();
+	}
+	
+	@Test
+	public void addTest() {
+	assertEquals(0, cal.add("1"));
+	} // 결과 test fail
+	
+	
+	@Test
+	public void addTest1() {
+		assertEquals(1, cal.add("1")); 
+				}
 }
