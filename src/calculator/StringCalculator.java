062b15f7bd54b691c1f440110b448950492a1971
sunyoong <haert2010@naver.com>
2022-05-28 16:53:09 +0900

    문자열 계산기 테스트 - 숫자 2개를 쉼표로 구분했을 때 두 숫자의 합

--- src/calculator/StringCalculator.java
+++ src/calculator/StringCalculator.java
@@ -4,12 +4,30 @@ import static org.junit.Assert.assertNotNull;
 
 public class StringCalculator {
 
-		public int add(String text) {
-			if(text==null || text.isEmpty()) {
-				return 0;
-			}
-			else {
-				return Integer.parseInt(text);
-			}
-		}
+	
+	
+//		public int add(String text) {
+//			if(text==null || text.isEmpty()) {
+//				return 0;
+//			}
+//			else {
+//				return Integer.parseInt(text);
+//			}
+//		}
+	
+		// 숫자 2개를 쉼표(,)로 구분했을 때 두 숫자의 합 반환
+	  	public int add2(String text) {
+	  		if(text==null || text.isEmpty()) {
+	  			return 0;
+	  		}
+	  		else {
+	  			String[] value = text.split(",");
+	  			int sum = 0;
+	  			for(String s : value) {
+	  				sum += Integer.parseInt(s);
+	  			
+	  			}
+	  			return sum;
+	  		}
+	  	}
 }
